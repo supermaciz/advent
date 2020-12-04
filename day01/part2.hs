2f2c3b209d31b76a398c2521f2e803ff75aa5faa
Mathieu Cizo <mathieu@pandascore.co>
2020-12-04 21:07:25 +0100

    ...

--- day01/part2.hs
+++ day01/part2.hs
@@ -4,18 +4,17 @@ convertInput :: String -> [Int]
 convertInput input = map (\x -> read x :: Int) $ lines input
 
 makeCombinations :: [Int] -> [(Int, Int, Int)]
-makeCombinations nbs = do
-  x <- nbs
-  y <- nbs
-  z <- nbs
-  return (x, y, z)
+makeCombinations nbs = [(x, y, z) | x <- nbs, y <- nbs, z <- nbs]
 
 goodNumbers :: (Int, Int, Int) -> Bool
 goodNumbers (x, y, z) = x + y + z == 2020
+
+isNotDuplicate :: (Int, Int, Int) -> Bool
+isNotDuplicate (x, y, z) = x /= y && y /= z
 
 main :: IO ()
 main = do
   input <- readFile "input"
   let cs = makeCombinations $ convertInput input
-   in let (a, b, c) = head $ dropWhile (not . goodNumbers) $ filter (\(x, y, z) -> x /= y && y /= z) cs
+   in let (a, b, c) = head $ dropWhile (not . goodNumbers) $ filter isNotDuplicate cs
        in print (a * b * c)
